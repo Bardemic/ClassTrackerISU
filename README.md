adc03c45566e29b59c5b403711fa0e54fdcdd905
Bardemic <134565367+Bardemic@users.noreply.github.com>
2025-01-24 13:39:11 -0600

    Add Scraping/ReadMe

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # ClassTrackerISU
 Get texts when a class becomes available at Iowa State
+
+
+## Setup
+- Create .env file with "ACCOUNT_SID" (from Twilio), "AUTH_TOKEN" (from Twilio), and "NUMBER_FROM" (from Twilio), "NUMBER_TO" (your own number, following the format "+{areacode}{number}", for example "+15555555555")
+- run "pip install twilio"
+- change class information to certain class (I will make it easier in the future)
+- run "npm run dev"
+- boom! running locally
--- main.py
+++ main.py
@@ -1,3 +1,30 @@
 from send_text import send_Text
+import requests
+import os
+from dotenv import load_dotenv
 
-#example: send_Text("+1ABC-DEF-GHIJ", "COMS 2280", "A")
+load_dotenv()
+
+url = "https://api.classes.iastate.edu/api/courses/search"
+payload = {
+    "courseId": "228",
+    "department": "COMS - Computer Science",
+    "academicPeriodId": "ACADEMIC_PERIOD-2025Spring",
+    "openSeats": False,
+}
+headers = {
+    "Content-Type": "application/json"
+}
+
+response = (requests.post(url, json=payload, headers=headers)).json()
+
+print(response.keys())
+
+sections = response["data"][0]["sections"]
+
+for section in sections:
+    if(section["openSeats"]):
+        print(section["number"])
+    
+
+#send_Text(os.environ.get("NUMBER_TO"), "COMS 2280", "A")
